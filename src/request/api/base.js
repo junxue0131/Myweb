376b3007eeef0277e6cf82ecb87efca36fb72009
xuejun <xuejun0131@gmail.com>
2020-08-14 19:17:55 +0800

    update:生产环境打包

--- src/request/api/base.js
+++ src/request/api/base.js
@@ -2,8 +2,8 @@
  * 接口域名的管理
  */
 const base = {    
-    url: 'http://192.168.43.66:8081',
-    // url: 'http://home.ziqiang.net.cn:8081'
+    // url: 'http://192.168.43.66:8081',
+    url: 'http://home.ziqiang.net.cn:8081'
 }
 
 export default base;
